573edd87894d2a0782e53d53a5ee23f44ef200f4
tamer998 <tameram998@gmail.com>
2022-12-25 20:31:35 +0200

    components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HaderComponent } from './hader/hader.component';
 
 const routes: Routes = [];
 
